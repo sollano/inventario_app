5aa11e9408a8cba4aff8fc53ea6ad51d1d27c389
sollano <sollanorb@gmail.com>
2018-03-10 19:08:53 -0300

    add function stuff

--- funs/acs.R
+++ funs/acs.R
@@ -1,3 +1,64 @@
+#' Amostragem Casual Simples
+#'
+#'Funcao para o processamento de inventario florestal utilizando a amostragem casual simples.
+#'@details 
+#' Essa funcao permite o processamento de um inventario florestal utilizando a amostragem casual estratificada,
+#' para n talhoes, considerando a populcao finita ou infinita.
+#' E possivel executar varios inventarios de uma vez utilizando uma variavel categorica indicada no parametro \code{.groups}().
+#'
+#' @param df Data frame a ser utilizado.
+#' @param Yi Nome entre aspas da variavel volume, ou variavel a ser amostrada.
+#' @param area_parcela  Nome entre aspas da variavel area da unidade amostral utilizada, em metros quadrados. Pode tambem ser um valor numerico.
+#' @param area_total Nome entre aspas da variavel area total, em ha. Pode tambem ser um vetor contendo os valores das areas dos estratos.
+#' @param .groups Parametro opcional. Utilizado quando se deseja fazer mais de um inventario por vez.
+#' deve-se indicar o nome entre aspas da(s) variavel(is) fatoriais a serem consideradas. 
+#' Caso seja mais de uma, deve-se inserir um vetor contendo os nomes das variaveis.
+#' Por exemplo: para um inventario casual simples para cada codigo genetico, assumindo que o nome
+#' da variavel que designa os codigos geneticos seja CODGEN, utiliza-se: \code{"CODGEN"}.
+#' @param idade Nome entre aspas da variavel idade. Parametro opcional. Calcula a media da idade fornecida.
+#' @param alpha Valor da significancia a ser utilizada no calculo de t-student. Padrao: \code{0.05}.
+#' @param erro Valor do erro minimo admitido no inventario, em porcentagem. Padrao: \code{10}.
+#' @param casas_decimais Numero de casas decimais que seram utilizdas no resultado final. Padrao: \code{4}.
+#' @param pop Populacao considerada nos calculos. Pode ser infinita(\code{"inf"}) ou finita (\code{"fin"}). Padrao: \code{"inf"}.
+#' @param tidy A tabela final deve ser tidy (organizada) ou nao? Padrao: \code{TRUE}.
+#' @return Dataframe contendo as informacoes sobre a amostragem.
+#' 
+#' @keywords Amostragem casual simples
+#' @references 
+#' CAMPOS, J. C. C.; LEITE, H. G. Mensuracao florestal: perguntas e respostas. 3a. ed. Vicosa: Editora UFV, 2013. 605 p.
+#' 
+#' SOARES, C. P. B.; NETO, F. D. P.; SOUZA, A. L. D. Dendrometria e Inventario Florestal. 2a. ed. Vicosa: UFV, 2012. 272 p.
+#' 
+#' @seealso outras funcoes de amostragem: 
+#'   \code{\link{ace}} para amostragem casual estratificada, e
+#'   \code{\link{as_diffs}} para amostragem sistematica.
+#' @export
+#' @examples
+#' library(forestr)
+#' data("ex2_mfr")
+#' data("ex3_mfr")
+#' data("ex4_mfr")
+#'
+#' # Deseja-se inventariar uma area, atingindo um erro de 20%.
+#' # Primeiro e realizdo um inventario piloto:
+#' # inventario piloto, considerando um erro de 20%, pop finita:
+#' acs(ex3_mfr, "VCC", "AREA_PARCELA", "AREA_TOTAL", erro = 20, pop = "fin")
+#'
+#' # Percebe-se que temos 10 amostras, mas precisamos de 25 para obter o erro desejado.
+#' # Apos amostrar novas parcelas, realiza-se o inventario definitivo:
+#' # inventario definitivo, considerando um erro de 20%, pop finita:
+#' acs(ex4_mfr, "VCC", "AREA_PARCELA", "AREA_TOTAL", erro = 20, pop = "fin")
+#'
+#' # O erro desejado foi alcancado.
+#' 
+#' # Valores de area podem ser numericos
+#' acs(ex4_mfr, "VCC", 3000, 46.8, erro = 20, pop = "fin")
+#' 
+#' # um inventario casual simples para cada talhao:
+#' acs(ex2_mfr, "VCC", "AREA_PARCELA", "AREA_TALHAO",.groups = "TALHAO" ,erro = 20, pop = "fin")
+#'
+#' @author Sollano Rabelo Braga \email{sollanorb@@gmail.com}
+
 acs <- function(df,Yi, area_parcela, area_total, idade, .groups, alpha = 0.05, erro = 10, casas_decimais=4, pop="inf",tidy=T){
   # checagem de variaveis ####
   
