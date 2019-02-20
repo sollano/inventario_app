a1d29da9e9f36f57dbd0734acc417e0781bc31e9
Sollano <sollanorb@gmail.com>
2019-02-20 11:31:31 -0300

    add logging

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(
           
           
           # Version ####
-          navbarPage("App Inventário Florestal 2.1.5",id="tab",
+          navbarPage("App Inventário Florestal 2.1.6",id="tab",
           # ####           
                      theme = "green_yeti2.css",
                      # theme = "green.css", # seleciona um tema contido na pasta www
