b9fcb12513e54f9592ee4ad732d7ad8a1026f684
Sollano <sollanorb@gmail.com>
2019-02-26 11:40:11 -0300

    better consistency, update ht checks

--- funs/consistency.R
+++ funs/consistency.R
@@ -1,10 +1,18 @@
-consistency <- function(df, dap, ht, parcela,lower=0.2, upper=10){
+consistency <- function(df, cap, dap, ht,parcela,especie,arvore,fuste, lower=0.2, upper=10){
   
   DF <- as.data.frame(df)
   
   # se DF nao for fornecido, nulo, ou  nao for dataframe, parar
   if(  missing(DF) || all(is.null(DF)) || all(is.na(DF)) || !is.data.frame(DF) ){  
     stop("DF not set", call. = F) 
+  }
+  
+  # se cap for fornecido, calcular DAP
+  if(  missing(cap) || is.null(cap) || is.na(cap) || cap == "" || is.null(DF[[cap]]) ){
+    
+  }else{  
+    DF$DAP <- DF[[cap]]/pi
+    dap <- "DAP"
   }
   
   # se dap nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
@@ -20,12 +28,39 @@ consistency <- function(df, dap, ht, parcela,lower=0.2, upper=10){
   
   # se ht nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
   if(  missing(ht) || is.null(ht) || is.na(ht) || ht == "" || is.null(DF[[ht]] ) ){  
-    stop("ht not set", call. = F) 
+    DF$HT <- DF[[dap]]
+    ht_ <- "HT"
+  }else{
+    ht_ <- ht
   }
   
   # se ht nao for numerico, parar
-  if(  !is.numeric(DF[[ht]] ) ){  
+  if(  !is.numeric(DF[[ht_]] ) ){  
     stop("ht column must be numeric", call. = F) 
+  }
+  
+  # se especie nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
+  if(  missing(especie) || is.null(especie) || is.na(especie) || especie == "" || is.null(df[[especie]]) ){  
+    DF$especie_rm <- "none"
+    ESPCC <- "especie_rm"
+  }else{
+    ESPCC <- especie
+  }
+  
+  # se arvore nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
+  if(  missing(arvore) || is.null(arvore) || is.na(arvore) || arvore == "" || is.null(df[[arvore]]) ){  
+    DF$arvore_rm <- "none"
+    ARVV <- "arvore_rm"
+  }else{
+    ARVV <- arvore
+  }
+  
+  # se fuste nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
+  if(  missing(fuste) || is.null(fuste) || is.na(fuste) || fuste == "" || is.null(df[[fuste]]) ){  
+    DF$fuste_rm <- "none"
+    FUSTT <- "fuste_rm"
+  }else{
+    FUSTT <- fuste
   }
   
   # add nomes das linhas como coluna numerica (numerica e importante caso se queira usar esta coluna como filtro futuramente)
@@ -42,53 +77,120 @@ consistency <- function(df, dap, ht, parcela,lower=0.2, upper=10){
     
   }
   
-  DAPname <- dap
-  HTname <- ht
+  sym <- rlang::sym
   
+  DAP <- sym(dap)
+  HT <- sym(ht_)
+  ESPECIE <- sym(ESPCC)
+  ARVORE <- sym(ARVV)
+  FUSTE <- sym(FUSTT)
   DF$rowid <- as.numeric(rownames(DF))
   
   y <-  DF %>% 
     group_by_at(vars(PARCC)) %>% 
-    summarise_at(vars(DAPname, HTname), funs(mean(.,na.rm=T), sd(.,na.rm=T) ) ) %>% 
+    rename(dap=!!dap, ht=!!ht_) %>% 
+    summarise_at(vars(dap,ht), 
+                 funs(
+                   mean(.,na.rm=T),
+                   sd(.,na.rm=T),
+                   mean_minus_3_sd = mean(.,na.rm=T) - sd(.,na.rm=T)*3,
+                   mean_plus_3_sd = mean(.,na.rm=T) + sd(.,na.rm=T)*3 ) ) %>% 
     full_join(DF,by=PARCC) %>% 
+    rename(dap=!!dap, ht=!!ht_) %>% 
     mutate(
       DAP_test = case_when(
-        .data[[DAPname]] <= .data[[paste(DAPname,"mean",sep="_") ]] - .data[[paste(DAPname,"sd",sep="_") ]]*3 ~ "DAP menor que media - 3 sd",
-        .data[[DAPname]] >  .data[[paste(DAPname,"mean",sep="_") ]] + .data[[paste(DAPname,"sd",sep="_") ]]*3 ~ "DAP maior que media + 3 sd",
-        !is.na( .data[[DAPname]] ) & .data[[DAPname]] < 1.3                                                   ~ "Dap menor que 1,3",
-        TRUE                                                                                                  ~ "ok"
+        dap <= dap_mean_minus_3_sd ~ "DAP menor que media + 3 sd",
+        dap >  dap_mean_plus_3_sd  ~ "DAP maior que media + 3 sd",
+        TRUE                       ~ "ok"
       ),
       
-      HT_test = ifelse(.data[[HTname]]  <=  .data[[paste(HTname,"mean",sep="_") ]] - .data[[paste(HTname,"sd",sep="_") ]]*3, "Altura menor que media - 3 sd", ifelse(.data[[HTname]]  >  .data[[paste(HTname,"mean",sep="_") ]] + .data[[paste(HTname,"sd",sep="_") ]]*3, "Altura maior que media + 3 sd", ifelse(is.na( .data[[DAPname]] ) & is.numeric( .data[[HTname]] ), "Arvore com altura e sem dap", "ok")       )  ),
-      
       HT_test = case_when(
-        .data[[HTname]]  <=  .data[[paste(HTname,"mean",sep="_") ]] - .data[[paste(HTname,"sd",sep="_") ]]*3 ~ "Altura menor que media - 3 sd",
-        .data[[HTname]]  >  .data[[paste(HTname,"mean",sep="_") ]] + .data[[paste(HTname,"sd",sep="_") ]]*3  ~ "Altura maior que media + 3 sd",
-        is.na( .data[[DAPname]] ) & is.numeric( .data[[HTname]] )                                            ~ "Arvore com altura e sem dap",
-        TRUE                                                                                                 ~ "ok"
+        is.na( dap ) & is.numeric( ht ) ~ "Arvore com altura e sem dap",
+        ht <= ht_mean_minus_3_sd        ~ "Altura menor que media - 3 sd",
+        ht >  ht_mean_plus_3_sd         ~ "Altura maior que media + 3 sd",
+        TRUE                            ~ "ok"
       ),
       
       ratio_test = case_when(
-        .data[[DAPname]]/.data[[HTname]] <= lower | .data[[DAPname]]/.data[[HTname]] >= upper ~ "Razao dap/ht ruim",
-        TRUE                                                                                  ~ "ok"  
+        (dap)/(ht) <= lower | (dap)/(ht) >= upper ~ "Razao dap/ht ruim",
+        TRUE                                      ~ "ok"  
+        
+      ),
+      especie_test = case_when(
+        
+        (!!ESPECIE) %in% c("", " ", "  ") | is.na(!!ESPECIE) ~ "Especie vazia",
+        stringr::str_sub(!!ESPECIE, 1)==" "                  ~ "Espaco vazio no inicio de especie", 
+        stringr::str_sub(!!ESPECIE,-1)==" "                  ~ "Espaco vazio no final de especie",
+        TRUE                                                 ~ "ok"
+        
+      ),
+      
+      arvore_test = case_when(
+        
+        (!!ARVORE) %in% c("", " ", "  ") | is.na(!!ARVORE) ~ "arvore vazia",
+        stringr::str_sub(!!ARVORE, 1)==" "                  ~ "Espaco vazio no inicio de arvore", 
+        stringr::str_sub(!!ARVORE,-1)==" "                  ~ "Espaco vazio no final de arvore",
+        TRUE                                                 ~ "ok"
+        
+      ),
+      
+      fuste_test = case_when(
+        
+        (!!FUSTE) %in% c("", " ", "  ") | is.na(!!FUSTE) ~ "fuste vazia",
+        stringr::str_sub(!!FUSTE, 1)==" "                ~ "Espaco vazio no inicio de fuste", 
+        stringr::str_sub(!!FUSTE,-1)==" "                ~ "Espaco vazio no final de fuste",
+        TRUE                                             ~ "ok"
+        
+      ),
+      
+      parcela_test = case_when(
+        
+        (!!sym(PARCC)) %in% c("", " ", "  ") | is.na(!!sym(PARCC)) ~ "parcela vazia",
+        stringr::str_sub(!!sym(PARCC), 1)==" "                     ~ "Espaco vazio no inicio de parcela", 
+        stringr::str_sub(!!sym(PARCC),-1)==" "                     ~ "Espaco vazio no final de parcela",
+        TRUE                                                       ~ "ok"
         
       )
+      
     ) %>% 
-    filter( !is.na(.data[[DAPname]]) | !is.na(.data[[HTname]])  ) %>% 
-    filter(DAP_test != "ok" | HT_test != "ok" | ratio_test != "ok") %>% 
+    filter( !is.na(dap) | !is.na(ht)  ) %>% 
+    filter(DAP_test != "ok" | HT_test != "ok" | ratio_test != "ok" | especie_test != "ok") %>% 
     # filter_at(vars( "DAP_test", "HT_test", "ratio_test" ), any_vars(. != "ok") ) %>% 
-    select(rowid, DAP_test, HT_test, ratio_test,  everything() ) %>% 
+    select(rowid, DAP_test, HT_test, ratio_test, especie_test, parcela_test,arvore_test,fuste_test,  everything(), -ht_mean_minus_3_sd,-ht_mean_plus_3_sd,-dap_mean_minus_3_sd,-dap_mean_plus_3_sd ) %>% 
     arrange(rowid) %>% 
     as.data.frame
   
+  # remover ht e testes relacionados, caso ele nao seja fornecido
+  if(  missing(ht) || is.null(ht) || is.na(ht) || ht == "" || is.null(DF[[ht]] ) ){  
+    y[c("ht", "HT_test", "ht_mean", "ht_sd", "ratio_test")] <- NULL
+    
+  }
+  
+  
   if(missing(parcela) || is.null(parcela) || is.na(parcela) || parcela == ""){
     
-    DF$parcela <- NULL
+    y[c("parcela", "parcela_test")] <- NULL
   }
+  
+  # se especie nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
+  if(  missing(especie) || is.null(especie) || is.na(especie) || especie == "" ){  
+    y[c("especie_rm", "especie_test")] <- NULL
+  }
+  
+  # se arvore nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
+  if(  missing(arvore) || is.null(arvore) || is.na(arvore) || arvore == "" ){  
+    y[c("arvore_rm", "arvore_test")] <- NULL
+  }
+  
+  # se fuste nao for fornecido, for igual "", nulo, ou  nao existir no dataframe, parar
+  if(  missing(fuste) || is.null(fuste) || is.na(fuste) || fuste == "" ){  
+    y[c("fuste_rm", "fuste_test")] <- NULL
+  }
+  
   
   if(nrow(y) == 0){
     z <- NULL
-    # warning("No inconsistencies were found. yay!",call. = F)
+    warning("No inconsistencies were found. yay!",call. = F)
   }else{
     z <- y
   }
@@ -98,3 +200,4 @@ consistency <- function(df, dap, ht, parcela,lower=0.2, upper=10){
   
   
 }
+
--- server.R
+++ server.R
@@ -1072,14 +1072,29 @@ shinyServer(function(input, output, session) {
     
     # Aqui a funcao nao ira rodar, caso essas condicoes sejam contrariadas
     #  req(data, is.numeric(data[[input$col.dap]]),is.numeric(data[[input$col.ht]]) )
+    
+    # Check numeric para cap ou dap
+    if(!is.null(input$col.cap) && !is.na(input$col.cap) ){
+      req(input$col.cap)
+      validate(check_numeric(input$col.cap, data, "cap"))
+    }else{
+      req(input$col.dap)
+      
+      validate(check_numeric(input$col.dap, data, "dap"))
+    }
+    
     validate(
-      need(input$col.dap,""),
-      need(input$col.ht,""),
-      check_numeric(input$col.dap, data, "dap"),
       check_numeric(input$col.ht, data, "ht")  )
     
+    
+    
     #htdapratio(data, dap = input$col.dap, ht = input$col.ht) 
-    consistency(data, dap = input$col.dap, ht = input$col.ht, parcela = input$col.parcelas) 
+    consistency(data, 
+                cap = input$col.cap, 
+                dap = input$col.dap, 
+                ht = input$col.ht, 
+                parcela = input$col.parcelas,
+                arvore = input$col.arvore ) 
   })
   output$consist_warning1 <- renderUI({
     req(input$run_consist==TRUE)
